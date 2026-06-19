fb10f0fcc76161da9e077684b0c5f3dc10c902ff
arnb-smnta <loveumearnab.2812000@gmail.com>
2026-06-19 18:01:22 +0530

    fix: hopefully fix the db close issue

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,6 +5,20 @@ import { app as expressApplication } from "./server";
 import { env } from "./env";
 import { checkDatabaseConnection } from "@repo/database";
 
+const DB_HEALTH_CHECK_INTERVAL_MS = 30_000;
+
+function startDatabaseHealthCheck() {
+  setInterval(async () => {
+    try {
+      await checkDatabaseConnection();
+      logger.debug("database connection OK");
+    } catch (err) {
+      logger.error("Database health check failed", { err });
+      throw err;
+    }
+  }, DB_HEALTH_CHECK_INTERVAL_MS);
+}
+
 async function init() {
   try {
     await checkDatabaseConnection();
@@ -13,6 +27,7 @@ async function init() {
     const PORT: number = env.PORT ? +env.PORT : 8000;
     server.listen(PORT, () => {
       logger.info(`http server is running on PORT ${PORT}`);
+      startDatabaseHealthCheck();
     });
   } catch (err) {
     logger.error("Startup failed (database unreachable?)", { err });
